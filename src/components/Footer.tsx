074eef9524f15fd23cfcb57d3ccb691264402734
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-04-10 21:14:38 -0700

    replaces default component with placeholder content

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+    return (
+        <footer>
+            <p className="text-center text-xs">© {new Date().getFullYear()} - <a href="https://github.com/alexl8819">alexl8819</a></p>
+        </footer>
+    )
+}
