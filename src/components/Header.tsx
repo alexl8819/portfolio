ac248dc5b9781200754139599188e6516c975d8e
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-06-11 15:02:08 -0700

    adds implementation of Header and Navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { Menu } from 'lucide-react';
+import debounce from 'debounce';
+import { useState, useEffect, memo } from 'react';
+
+import { Button } from './ui/Button';
+import Navbar from './Navbar';
+
+const Header = () => {
+	const [open, setOpen] = useState<boolean>(false);
+
+	const toggle = () => setOpen(open ? false : true);
+	
+	useEffect(() => {
+		const forceClose = () => setOpen(false);
+
+		const debouncedForceClose = debounce(forceClose, 100);
+
+		window.addEventListener('resize', debouncedForceClose);
+
+		return () => {
+			window.removeEventListener('resize', debouncedForceClose);
+		}
+	}, []);
+    
+	return (
+        <header className="xl:hidden fixed top-0 left-0 right-0 z-50 bg-white/40 backdrop-blur-sm border-b border-zinc-100">
+        	<div className="flex items-center justify-end h-16 xl:h-20 px-4 md:px-12">
+				<div className="w-full md:w-0 xl:w-28 flex justify-end md:justify-start">
+					<Button className='cursor-pointer xl:hidden z-20' onClick={toggle} label="navigation menu">
+						<Menu />
+					</Button>
+					{
+						open ? (
+							<div className='min-h-screen w-full py-8 px-8 fixed top-0 left-0 bg-white z-0'>
+								<Navbar toggle={toggle} shouldOpen={open} />
+							</div>
+						) : null
+					}
+				</div>
+			</div>
+    	</header>
+	)
+};
+
+export default memo(Header);
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { memo, type FC } from 'react';
+import { 
+  User,
+  FileUser,
+  FolderGit,
+  ShieldUser,
+  Send
+} from 'lucide-react';
+
+import { VariantType, Button } from './ui/Button';
+import { updatePosition } from '../stores/page';
+import { scrollTo } from '../util';
+
+interface NavbarProps {
+	shouldOpen: boolean,
+	toggle?: Function
+}
+
+const Navbar: FC<NavbarProps> = ({ shouldOpen, toggle }) => {
+	const scroll = (section: string) => {
+		return () => {
+			scrollTo(section);
+			updatePosition(section);
+
+			if (typeof toggle === 'function') {
+				toggle();
+			}
+		}
+	}
+
+	return (
+		<nav className={`${shouldOpen ? 'flex mt-20 xl:mt-24' : 'hidden'} items-start my-4 md:flex flex-col w-full`}>
+			<ol className='list-none mb-8 w-full flex flex-col xl:justify-center items-start xl:items-center space-y-12'>
+				<li>
+					<Button className={`flex flex-row xl:flex-col items-start xl:items-center font-dm-sans text-sm md:text-base font-medium text-zinc-700/80 hover:text-zinc-800 transition-colors px-3 py-1.5 rounded-lg`} onClick={scroll('#projects')}>
+              			<FolderGit className='stroke-[1.5] inline-block mr-3 xl:mr-0 xl:mb-1.5' /> Projects
+            		</Button>
+				</li>
+            	<li>
+					<Button className={`flex flex-row xl:flex-col items-start xl:items-center font-dm-sans text-sm md:text-base font-medium text-zinc-700/80 hover:text-zinc-800 transition-colors px-3 py-1.5 rounded-lg`} onClick={scroll('#skills')}>
+              			<FileUser className='stroke-[1.5] inline-block mr-3 xl:mr-0 xl:mb-1.5' /> Skills
+            		</Button>
+				</li>
+				<li>
+					<Button className={`flex flex-row xl:flex-col items-start xl:items-center font-dm-sans text-sm md:text-base font-medium text-zinc-700/80 hover:text-zinc-800 transition-colors px-3 py-1.5 rounded-lg`} onClick={scroll('#certifications')}>
+              			<ShieldUser className='stroke-[1.5] inline-block mr-3 xl:mr-0 xl:mb-1.5' /> Certifications
+            		</Button>
+				</li>
+				<li>
+					<Button className={`flex flex-row xl:flex-col items-start xl:items-center font-dm-sans text-sm md:text-base font-medium text-zinc-700/80 hover:text-zinc-800 transition-colors px-3 py-1.5 rounded-lg`} onClick={scroll('#about')}>
+              			<User className='stroke-[1.5] inline-block mr-3 xl:mr-0 xl:mb-1.5' /> About
+            		</Button>
+				</li>
+				<li>
+					<Button onClick={scroll('#contact')} variant={VariantType.Outline} className={`flex flex-row xl:flex-col items-start xl:items-center font-dm-sans text-sm md:text-base justify-center bg-black text-white font-bold`}>
+            			<Send className='stroke-[1.5] inline-block mr-3 xl:mr-0 xl:mb-1.5' /> Contact Me
+          			</Button>
+				</li>
+			</ol>
+        </nav>
+	)
+}
+
+export default memo(Navbar);
