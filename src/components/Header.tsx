8fdfb032aaf0d3ec0eb20b8272a7487c2d45a94c
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-06-15 15:40:37 -0700

    fix: restore navbar toggle due to missing onClick to onPress change in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
         <header className="xl:hidden fixed top-0 left-0 right-0 z-50 bg-white/40 backdrop-blur-sm border-b border-zinc-100">
         	<div className="flex items-center justify-end h-16 xl:h-20 px-4 md:px-12">
 				<div className="w-full md:w-0 xl:w-28 flex justify-end md:justify-start">
-					<Button className='cursor-pointer xl:hidden z-20' onClick={toggle} label="navigation menu">
+					<Button className='cursor-pointer xl:hidden z-20' onPress={toggle} label="navigation menu">
 						<Menu />
 					</Button>
 					{
