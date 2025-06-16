478fb0a549097199985f7b8ba9604abd80db865a
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-06-15 20:14:29 -0700

    adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { version } from '../../package.json';
 const Footer = () => {
     return (
         <footer className='py-4'>
-            <p className="text-center text-xs">© Copyright {new Date().getFullYear()} — <a href="https://github.com/alexl8819">alexl8819</a> v{version}</p>
+            <p className="text-center text-xs">© Copyright {new Date().getFullYear()} v{version} — <a href="https://github.com/alexl8819">alexl8819</a></p>
         </footer>
     );
 }
