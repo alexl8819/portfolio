6cfbe076e62a69e2e156675cf3cd7f4837d6d7fd
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-07-01 01:47:56 -0700

    uses custom domain for send endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_route53_zone" "main" {
+  name         = var.cors_domain
+  private_zone = false
+}
+
 resource "aws_iam_role" "lambda_exec" {
   name               = "lambda_send_email_role"
   assume_role_policy = jsonencode({
@@ -66,11 +71,39 @@ resource "aws_apigatewayv2_api" "http_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = [var.cors_domain]
+    allow_origins = ["https://portfolio.${var.cors_domain}"]
     allow_methods = ["POST", "OPTIONS"]
     allow_headers = ["Content-Type", "Authorization"]
     expose_headers = ["Content-Length"]
     max_age        = 3600
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "api.${var.cors_domain}"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_name
+  type    = element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_type
+  zone_id = data.aws_route53_zone.main.zone_id
+  records = [element(tolist(aws_acm_certificate.cert.domain_validation_options), 0).resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
+resource "aws_apigatewayv2_domain_name" "custom" {
+  domain_name = "api.${var.cors_domain}"
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
   }
 }
 
@@ -102,13 +135,32 @@ resource "aws_apigatewayv2_stage" "default_stage" {
   auto_deploy = true
 
   route_settings { # this doesn't work, only applies to v1
-    route_key = "POST /send"
+    route_key              = "POST /send"
     throttling_burst_limit = 1
-    throttling_rate_limit = 1
+    throttling_rate_limit  = 1
   }
 }
 
+resource "aws_apigatewayv2_api_mapping" "mapping" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  domain_name = aws_apigatewayv2_domain_name.custom.id
+  stage       = aws_apigatewayv2_stage.default_stage.name
+}
+
+resource "aws_route53_record" "api_gateway" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "api.${var.cors_domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.custom.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.custom.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
 output "api_endpoint" {
-  value = aws_apigatewayv2_api.http_api.api_endpoint
+  value = "https://${aws_apigatewayv2_domain_name.custom.domain_name}/send"
 }
 
